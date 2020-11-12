52b1ae33655093ee78a6d65ea62c7a8d3c0cdf80
Christopher Rhodes <chrhodes@cox.net>
2020-11-11 16:14:53 -0800

    Session 1 Content

---
+++ Christopher/Session1/ConsoleApp1-Core/ConsoleApp1-Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
